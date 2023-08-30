a15019d7ed5f046ee0e1a0a1f237f55b0b070f10
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-30 22:42:43 +0300

    Setar AWS profider cu profil

--- 01/main.tf
+++ 01/main.tf
@@ -6,3 +6,10 @@ terraform {
     }
   }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  shared_config_files = ["~/.aws/config"]
+  shared_credentials_files = [ "~/.aws/credentials" ]
+  profile = "default"
+}

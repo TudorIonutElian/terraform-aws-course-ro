524518f0ab3ca6822fbaa4b78321e9a677da9fcb
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-30 20:41:55 +0300

    Adaugare terraform provider

--- update.sh
+++ update.sh
@@ -1,17 +1,3 @@
-echo -e "==================================================";
-echo -e "=== MOVING to terraform-aws-course-ro directory ==";
-echo -e "===================================================";
-
-cd terraform-aws-course-ro;
-sleep 2;
-
-git checkout master;
-
-echo -e "===============================================";
-echo -e "========== SWITCHING to master branch ===========";
-echo -e "===============================================";
-
-
-git pull;
-
-eval "terraform --version";
+command='ssh root@136.244.90.208 ./update.sh'
+#command='ssh -i id_rsa root@45.76.95.196'
+eval $command;

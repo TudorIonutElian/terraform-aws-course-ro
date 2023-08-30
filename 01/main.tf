a112f9ccd92710795f8c60bdce5356db403a28a4
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-08-30 23:25:01 +0300

    Adaugare tags

--- 01/main.tf
+++ 01/main.tf
@@ -17,4 +17,9 @@ provider "aws" {
 # Prima noastra resursa AWS
 resource "aws_vpc" "primul_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "my_first_tag" = "my_first_tag_value"
+    "my_second_tag" = "my_second_tag_value"
+  }
 }

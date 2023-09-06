d43ea7a558ef5000353713d4693a2fe2ed32a42b
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-06 22:18:30 +0300

    Fix default

--- cloud/main.tf
+++ cloud/main.tf
@@ -19,7 +19,6 @@ terraform {
 provider "aws" {
   shared_config_files = ["~/.aws/config"]
   shared_credentials_files = [ "~/.aws/credentials" ]
-  profile = "default"
 }
 
 resource "aws_iam_user" "my_user" {

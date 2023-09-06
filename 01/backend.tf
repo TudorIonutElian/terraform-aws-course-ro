5c355c4e37613bd9ed349fe6193a131ff8f5ceb5
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-06 20:22:02 +0300

    Add lock table

--- 01/backend.tf
+++ 01/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terraform-aws-course-ro-central-state"
     key = "terraform.tfstate"
     region = "eu-central-1"
+    dynamodb_table = "terraform-aws-course-ro-db-lock-table"
   }
 }

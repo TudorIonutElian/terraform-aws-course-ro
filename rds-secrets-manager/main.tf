43e55879b9170358be22adec2380702a41d759e3
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-07 20:30:37 +0300

    Add secrets manager with local.

--- rds-secrets-manager/main.tf
+++ rds-secrets-manager/main.tf
@@ -26,8 +26,8 @@ resource "aws_db_instance" "education" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  username               = database_credentials.username
-  password               = database_credentials.password
+  username               = local.database_credentials.username
+  password               = local.database_credentials.password
   publicly_accessible    = true
   skip_final_snapshot    = true
 }

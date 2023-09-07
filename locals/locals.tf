79bc62d25bfa26f1b289e69df3a239fb6b8d1610
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-07 18:47:48 +0300

    Adding locals

---
+++ locals/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  tag = "my-local-tag"
+}
---
+++ locals/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  shared_config_files = ["~/.aws/config"]
+  shared_credentials_files = [ "~/.aws/credentials" ]
+  profile = "default"
+}
+
+resource "aws_iam_user" "my_user" {
+  name = "my-user-nume-${count.index}"
+  path = "/"
+
+  count = 3
+
+  tags = {
+    "tag" = var.locals.tag
+  }
+}
